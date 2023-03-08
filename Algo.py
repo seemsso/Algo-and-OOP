d2a0232e25b8ac2deca5fa7dae29a6a73f5eda38
seemsso <W8something@yandex.com>
2023-03-08 18:22:40 +0500

    choise sort realisation

--- Algo.py
+++ Algo.py
@@ -1,23 +1,48 @@
 """ BINARY SEARCH """
 
-def binary_search(list, item):
-    low = 0
-    res = 0
-    high = len(list) - 1
+# def binary_search(list, item):
+#     low = 0
+#     res = 0
+#     high = len(list) - 1
+#
+#     while low <= high:
+#         res += 1
+#         mid = int((low + high) / 2)
+#         guess = list[mid]
+#         if guess == item:
+#             return f"Score iteration: {res} for value {mid}"
+#         elif guess > item:
+#             high = mid - 1
+#         else:
+#             low = mid + 1
+#
+#     return None
+#
+# lst = [i for i in range(100)]
+# print(binary_search(lst, 32))
 
-    while low <= high:
-        res += 1
-        mid = int((low + high) / 2)
-        guess = list[mid]
-        if guess == item:
-            return f"Score iteration: {res} for value {mid}"
-        elif guess > item:
-            high = mid - 1
-        else:
-            low = mid + 1
 
-    return None
+""" CHOICE SORT """
 
-lst = [i for i in range(100)]
-print(binary_search(lst, 32))
-
+# def find_small(arr):
+#     small = arr[0]
+#     small_i = 0
+#
+#     for i in range(1, len(arr)):
+#         if arr[i] < small:
+#             small = arr[i]
+#             small_i = i
+#     return small_i
+#
+#
+#
+# def sort_lst(arr):
+#     newArr = []
+#     for i in range(len(arr)):
+#         small = find_small(arr)
+#         newArr.append(arr.pop(small))
+#     return newArr
+#
+#
+# a = [1, 3, 2, 10, 25, 7]
+# print(sort_lst(a)) # =>  [1, 2, 3, 7, 10, 25]
