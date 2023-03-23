dda6e3bd871c3e4959cf3c35bf12f63defd24c70
seemsso <W8something@yandex.com>
2023-03-23 15:57:40 +0500

    magic method __new__

--- oop.py
+++ oop.py
@@ -343,87 +343,92 @@
 # print(mb.get_config())
 
 
-class Cart:
-
-    def __init__(self):
-        self.goods = []
-
-    def add(self, gd):
-        self.goods.append(gd)
-
-    def remove(self, indx):
-        self.goods.pop(indx)
-
-    def get_list(self):
-        return [f'{x.name}: {x.price}' for x in self.goods]
-
-
-class Table:
-
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
-
-
-class TV:
-
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
-
-
-class Notebook:
-
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
-
-
-class Cup:
-
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
-
-
-cart = Cart()
-cart.add(TV('Samsung', 69999))
-cart.add(TV('Sony', 49990))
-cart.add(Table('IKEA', 14990))
-cart.add(Notebook('Macbook M1', 89990))
-cart.add(Notebook('Xiaomi Redmibook', 79000))
-cart.add(Cup('Кружка 300мл с рисунком', 299))
-
-print(cart.get_list())
-
-
+# class Cart:
+#
+#     def __init__(self):
+#         self.goods = []
+#
+#     def add(self, gd):
+#         self.goods.append(gd)
+#
+#     def remove(self, indx):
+#         self.goods.pop(indx)
+#
+#     def get_list(self):
+#         return [f'{x.name}: {x.price}' for x in self.goods]
+#
+#
+# class Table:
+#
+#     def __init__(self, name, price):
+#         self.name = name
+#         self.price = price
+#
+#
+# class TV:
+#
+#     def __init__(self, name, price):
+#         self.name = name
+#         self.price = price
+#
+#
+# class Notebook:
+#
+#     def __init__(self, name, price):
+#         self.name = name
+#         self.price = price
+#
+#
+# class Cup:
+#
+#     def __init__(self, name, price):
+#         self.name = name
+#         self.price = price
+#
+#
+# cart = Cart()
+# cart.add(TV('Samsung', 69999))
+# cart.add(TV('Sony', 49990))
+# cart.add(Table('IKEA', 14990))
+# cart.add(Notebook('Macbook M1', 89990))
+# cart.add(Notebook('Xiaomi Redmibook', 79000))
+# cart.add(Cup('Кружка 300мл с рисунком', 299))
+#
+# print(cart.get_list())
 
 
-class ListObject:
-
-    def __init__(self, data: str):
-        self.data = data
-        self.next_obj = None
-
-    def link(self, obj):
-        self.next_obj = obj
 
 
-lst_in = ['1. Первые шаги в ООП',
-          '1.1 Как правильно проходить этот курс',
-          '1.2 Концепция ООП простыми словами',
-          '1.3 Классы и объекты. Атрибуты классов и объектов',
-          '1.4 Методы классов. Параметр self',
-          '1.5 Инициализатор init и финализатор del',
-          '1.6 Магический метод new. Пример паттерна Singleton',
-          '1.7 Методы класса (classmethod) и статические методы (staticmethod)']
+# class ListObject:
+#
+#     def __init__(self, data: str):
+#         self.data = data
+#         self.next_obj = None
+#
+#     def link(self, obj):
+#         self.next_obj = obj
+#
+#
+# lst_in = ['1. Первые шаги в ООП',
+#           '1.1 Как правильно проходить этот курс',
+#           '1.2 Концепция ООП простыми словами',
+#           '1.3 Классы и объекты. Атрибуты классов и объектов',
+#           '1.4 Методы классов. Параметр self',
+#           '1.5 Инициализатор init и финализатор del',
+#           '1.6 Магический метод new. Пример паттерна Singleton',
+#           '1.7 Методы класса (classmethod) и статические методы (staticmethod)']
+#
+# head_obj = ListObject(lst_in[0])
+# obj = head_obj
+# for i in range(1, len(lst_in)):
+#     cur = ListObject(lst_in[i])
+#     obj.link(cur)
+#     obj = cur
 
-head_obj = ListObject(lst_in[0])
-obj = head_obj
-for i in range(1, len(lst_in)):
-    cur = ListObject(lst_in[i])
-    obj.link(cur)
-    obj = cur
+""" Magic Method __new__ """
+
+
+
 
 
 
