cd4c489289617701c4362e433820a7c51c32bd68
seemsso <W8something@yandex.com>
2023-04-21 17:47:27 +0500

    OOP Access public,private,protected

--- oop.py
+++ oop.py
@@ -558,36 +558,36 @@
 """Access public,private,protected"""
 
 
-class Clock:
-    cur = []
-
-    def __init__(self, time=0):
-        if self.cur:
-            el = len(self.cur) - 1
-            time = el
-        self.__time = time
-        self.cur.append(time)
-
-    def set_time(self, tm):
-        if self.__check_time(tm):
-            self.__time = tm
-
-    def get_time(self):
-        return self.__time
-
-    @classmethod
-    def __check_time(cls, tm):
-        if type(tm) == int and (0 <= tm < 100_000):
-            return True
-        else:
-            return False
-
-
-clock = Clock(4530)
-clock.set_time(15)
-print(clock.get_time())  # 15
-clock.set_time(100000)
-clock.set_time(-1)
-clock.set_time('2')
-clock.set_time(0.1)
-print(clock.get_time())
+# class Clock:
+#     cur = []
+#
+#     def __init__(self, time=0):
+#         if self.cur:
+#             el = len(self.cur) - 1
+#             time = el
+#         self.__time = time
+#         self.cur.append(time)
+#
+#     def set_time(self, tm):
+#         if self.__check_time(tm):
+#             self.__time = tm
+#
+#     def get_time(self):
+#         return self.__time
+#
+#     @classmethod
+#     def __check_time(cls, tm):
+#         if type(tm) == int and (0 <= tm < 100_000):
+#             return True
+#         else:
+#             return False
+#
+#
+# clock = Clock(4530)
+# clock.set_time(15)
+# print(clock.get_time())  # 15
+# clock.set_time(100000)
+# clock.set_time(-1)
+# clock.set_time('2')
+# clock.set_time(0.1)
+# print(clock.get_time())
